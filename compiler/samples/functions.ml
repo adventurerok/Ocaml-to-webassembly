62c787bc70dca5cd716e34594bf98e18ad6d71f7
Paul Durbaba <pd452@cam.ac.uk>
2019-12-02 01:16:57 +0000

    Create function sample

---
+++ compiler/samples/functions.ml
@@ -0,0 +1,15 @@
+let double n = 2 * n
+
+let sum x y = x + y
+
+let rec fib n = if n = 0 then 1 else fib (n - 1)
+
+let add3 n =
+  let add1 n = n + 1 in
+  add1 (add1 (add1 n))
+
+let mult m n =
+  let rec addm t a =
+    if t = 0 then a
+    else addm (t-1) (a + m) in
+  addm n 0
