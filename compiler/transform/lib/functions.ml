b76942ff19f151d06aebc394bb770d7318c07b5c
Paul Durbaba <pd452@cam.ac.uk>
2020-01-27 23:45:42 +0000

    Function transform using traverser

--- compiler/transform/lib/functions.ml
+++ compiler/transform/lib/functions.ml
@@ -47,6 +47,17 @@ type func_data = {
 
 type type_map = (string, scheme_type) Map.Poly.t
 
+type trav_state = {
+  mutable fnames : func_names;
+  mutable funcs : func_data list;
+  traverser: ((trav_state, trav_input, unit) Traverse_ast.traverser)
+}
+
+and trav_input = {
+  next_name: string option;
+  locals: type_map;
+}
+
 let add_vars map vars =
   List.fold ~init:map ~f:(fun m (tv, t) -> Map.Poly.set m ~key:tv ~data:t) vars
 
@@ -55,10 +66,6 @@ let rec remove_apps name count =
     remove_apps (String.chop_suffix_exn name ~suffix:"-app") (count + 1)
   else (name, count)
 
-type state = {
-  mutable fnames : func_names;
-  mutable funcs : func_data list
-}
 
 (* Gives export names to the functions to export *)
 (* Current criteria: named functions with all their arguments (most -apps on the end), with no cvars *)
@@ -80,73 +87,9 @@ let select_export_functions fnames funcs =
 (* So on an expression, we'll need the modified expression, and the map of id to function *)
 (* Globals: Not in closure (global variables or arguments), Locals: In closure *)
 
-let rec func_transform_expr ?next_name:(next_name=None) (state: state) (locals: type_map) (expr: texpression) =
-  let desc =
-    match expr.texp_desc with
-    | Texp_ident(x) -> Texp_ident(x)
-    | Texp_constant(c) -> Texp_constant(c)
-    | Texp_let (rf, tvb_list, e) -> func_transform_let state locals rf tvb_list e
-    | Texp_fun (p, e) -> func_transform_func next_name state locals expr.texp_type p e
-    | Texp_apply (a, blist) ->
-        let a' = func_transform_expr state locals a in
-        let blist' = List.map blist ~f:(func_transform_expr state locals) in
-        (Texp_apply(a', blist'))
-    | Texp_match (e, cases) -> func_transform_match state locals e cases
-    | Texp_tuple(ls) ->
-        let ls' = List.map ls ~f:(func_transform_expr state locals) in
-        (Texp_tuple(ls'))
-    | Texp_construct (name, ls) ->
-        let ls' = List.map ls ~f:(func_transform_expr state locals) in
-        (Texp_construct(name, ls'))
-    | Texp_ifthenelse (i, t, e_opt) ->
-        let i' = func_transform_expr state locals i in
-        let t' = func_transform_expr state locals t in
-        (match e_opt with
-        | Some(e) ->
-            let e' = func_transform_expr state locals e in
-            (Texp_ifthenelse(i', t', Some(e')))
-        | None ->
-            (Texp_ifthenelse(i', t', None)))
-    | Texp_while(cond, inner) ->
-        let cond' = func_transform_expr state locals cond in
-        let inner' = func_transform_expr state locals inner in
-        (Texp_while(cond', inner'))
-    | Texp_for(var_opt, min, max, dir, inner) ->
-        let min' = func_transform_expr state locals min in
-        let max' = func_transform_expr state locals max in
-        let inner' = func_transform_expr state locals inner in
-        (Texp_for(var_opt, min', max', dir, inner'))
-    | Texp_sequence(a, b) ->
-        let a' = func_transform_expr state locals a in
-        let b' = func_transform_expr state locals b in
-        (Texp_sequence(a', b'))
-  in {expr with texp_desc = desc}
-
-and func_transform_value_bindings state locals rf tvb_list =
-  let vars = List.concat (List.map tvb_list ~f:(fun tvb -> tvb.tvb_pat.tpat_vars)) in
-  let locals' = List.fold ~init:locals ~f:(fun a (v, t) -> Map.Poly.set a ~key:v ~data:t) vars in
-  let inner_locals =
-    (match rf with
-    | Asttypes.Nonrecursive -> locals
-    | Asttypes.Recursive -> locals')
-  in
-  let tvb_list' = List.map tvb_list ~f:(fun tvb ->
-    let next_name =
-      match tvb.tvb_pat.tpat_desc with
-      | Tpat_var(name) -> Some(name)
-      | _ -> None
-    in
-    let e' = func_transform_expr ~next_name:next_name state inner_locals tvb.tvb_expr in
-    {tvb with tvb_expr = e'})
-  in
-  (locals', tvb_list')
-
-and func_transform_let state locals rf tvb_list e =
-  let (locals', tvb_list') = func_transform_value_bindings state locals rf tvb_list in
-  let e' = func_transform_expr state locals' e in
-  (Texp_let(rf, tvb_list', e'))
-
-and func_transform_func next_name state locals typ pat expr =
+and func_transform_func state input typ pat expr =
+  let locals = input.locals in
+  let next_name = input.next_name in
   let (fnames1, fname, app_name) =
     match next_name with
     | Some(name) -> add_named_func state.fnames name
@@ -155,7 +98,12 @@ and func_transform_func next_name state locals typ pat expr =
   let () = state.fnames <- fnames1 in
   let locals' = List.fold ~init:locals ~f:(fun a (t, v) -> Map.Poly.set a ~key:t ~data:v) pat.tpat_vars in
   let next_name' = Some(app_name) in
-  let expr_trans = func_transform_expr ~next_name:next_name' state locals' expr in
+  let input1 = {
+    next_name = next_name';
+    locals = locals'
+  }
+  in
+  let (_, expr_trans) = Traverse_ast.traverse_expr state.traverser state input1 expr in
   let vars_used = texpression_free_vars expr in
   let closure_vars = Map.Poly.filter_keys vars_used ~f:(Map.Poly.mem locals) in
   let closure_list = Map.to_alist closure_vars in
@@ -191,43 +139,81 @@ and func_transform_func next_name state locals typ pat expr =
   let () = state.funcs <- fdata :: state.funcs in
   (Texp_apply(fident, [tuple]))
 
-and func_transform_structure_item state (si: tstructure_item) =
-  let desc =
-    match si.tstr_desc with
-    | Tstr_eval(e) ->
-        let e' = func_transform_expr state Map.Poly.empty e in
-        (Tstr_eval(e'))
-    | Tstr_value (rf, tvb_list) ->
-        let (_, tvb_list') = func_transform_value_bindings state Map.Poly.empty rf tvb_list in
-        (Tstr_value(rf, tvb_list'))
-    | Tstr_type -> (si.tstr_desc)
-  in {si with tstr_desc = desc}
 
-and func_transform_match state locals e cases =
-  let e' = func_transform_expr state locals e in
-  let cases' = List.map cases ~f:(fun case ->
-    let locals' = List.fold ~init:locals ~f:(fun a (v, t) -> Map.Poly.set a ~key:v ~data:t) case.tc_lhs.tpat_vars in
-    let rhs' = func_transform_expr state locals' case.tc_rhs in
-    {case with tc_rhs = rhs'}
-  )
+
+let trav_step_input _ input = {
+  input with
+  next_name = None
+}
+
+let trav_pre_expr state input expr =
+  match expr.texp_desc with
+  | Texp_fun(pat, inner) ->
+      let desc' = func_transform_func state input expr.texp_type pat inner in
+      (input, {expr with texp_desc = desc'})
+  | _ -> (input, expr)
+
+let trav_pre_let _ input (rf, tvb_lst, _) =
+  let vars = List.concat (List.map tvb_lst ~f:(fun tvb -> tvb.tvb_pat.tpat_vars)) in
+  let locals = input.locals in
+  let locals' = List.fold ~init:locals ~f:(fun a (v, t) -> Map.Poly.set a ~key:v ~data:t) vars in
+  let inner_locals =
+    (match rf with
+    | Asttypes.Nonrecursive -> locals
+    | Asttypes.Recursive -> locals')
   in
-  (Texp_match(e', cases'))
+  ({input with locals = inner_locals}, {input with locals = locals'}, tvb_lst)
 
-and func_transform_structure (struc: tstructure) =
-  let fnames = {
-    temp_count = 0;
-    names = Set.Poly.empty
+let trav_mid_value_binding _ input _ tvb =
+    let next_name =
+      match tvb.tvb_pat.tpat_desc with
+      | Tpat_var(name) -> Some(name)
+      | _ -> None
+    in
+    let input1 = {
+      input with
+      next_name = next_name
+    }
+    in
+    (input1, tvb)
+
+let trav_mid_case _ input _ case =
+  let locals = input.locals in
+  let locals' = List.fold ~init:locals ~f:(fun a (v, t) -> Map.Poly.set a ~key:v ~data:t) case.tc_lhs.tpat_vars in
+  let input1 = {
+    input with
+    locals = locals'
   }
   in
+  (input1, case)
+
+let func_transform_traverse : (trav_state, trav_input, unit) Traverse_ast.traverser =
+  {
+    (Traverse_ast.no_output_traverser trav_step_input) with
+    pre_let = trav_pre_let;
+    mid_value_binding = trav_mid_value_binding;
+    pre_expr = trav_pre_expr;
+    mid_case = trav_mid_case
+  }
+
+let func_transform_structure (struc: tstructure) =
   let state = {
-    fnames = fnames;
-    funcs = []
+    fnames = {
+      temp_count = 0;
+      names = Set.Poly.empty
+    };
+    funcs = [];
+    traverser = func_transform_traverse;
   }
   in
-  let si_list = List.map struc ~f:(func_transform_structure_item state) in
+  let input = {
+    next_name = None;
+    locals = Map.Poly.empty;
+  }
+  in
+  let (_, si_list) = Traverse_ast.traverse_structure state.traverser state input struc in
   let funcs' = select_export_functions state.fnames state.funcs in
   (funcs', si_list)
-
 
 let func_data_to_string fdata =
   let pstr = tpattern_to_string fdata.fd_pat in
